e9ad754c2725e47d9742c2726e7778695a76a340
RullerBlate <115470262+RullerBlate@users.noreply.github.com>
2022-10-26 17:30:24 +0300

    Summary

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    bool inAir;
+    bool isGrounded;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float a = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(a * speed, rb.velocity.y);
+
+        if (a != 0 && inAir == false)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+            animator.SetBool("IsWalking", false);
+
+        if (Input.GetKey(KeyCode.Space) && !inAir)
+        {
+            inAir = true;
+            rb.AddForce(new Vector2(0, 350));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            inAir = false;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            Destroy(gameObject);
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Coin"))
+            Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isGoingRight = true;
+
+    void Update()
+    {
+        if (isGoingRight)
+        transform.Translate(1 * Time.deltaTime, 0, 0);
+        else
+        transform.Translate(-1 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("RightWall"))
+            isGoingRight = false;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("LeftWall"))
+            isGoingRight = true;
+    }
+}
